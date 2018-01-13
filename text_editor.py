002e4bfa56a0c5990b2ecb51400848fe2bdc346e
Jude Kuti <judekuti@gmail.com>
2018-01-13 01:57:04 -0600

    Building a text editor with tkinter

---
+++ text_editor.py
@@ -0,0 +1,13 @@
+from tkinter import *
+import tkinter.filedialog
+
+
+class TextEditor:
+
+    @staticmethod
+    def quit_app(event=None):
+        root.quit()
+
+    def open_file(self, event=None):
+
+        txt_file =
